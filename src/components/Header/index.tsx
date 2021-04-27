aa99e9b6dd882d2d5e1c191d93feb683ef638264
Joao Augusto Oliveira dos Santos <joaoaugustofurg@gmail.com>
2021-04-27 16:36:34 -0300

    :sparkles: add api fake

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss'; 
+// o .module limita a estilização para esse componente.
+
+export default function Header() {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+    });
+
+    return (
+        <header className={styles.headerContainer}>
+            <img src="/logo.svg" alt="Podcastr"/>
+
+            <p>O melhor para você ouvir, sempre</p>
+ 
+            <span>{currentDate}</span>
+        </header>
+    )
+}
