fd01ff678c55518b3a9b06ddb937c2ceffb143aa
Joao Augusto Oliveira dos Santos <joaoaugustofurg@gmail.com>
2021-05-04 12:20:17 -0300

    deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
+
+const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:3333/";
+console.log(process.env.REACT_APP_API_URL);
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_LINK_API || "http://localhost:3000/"
-})
+  baseURL,
+});
